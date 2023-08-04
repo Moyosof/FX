98e75926fa0e3dc1be802d6693ec4e607a8dce42
Tope-stack <yusufftope70@gmail.com>
2023-08-04 08:16:42 +0100

    Cloduinary Settings Done

---
+++ FX.Application/FileService/IFileService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FX.Application.FileService
+{
+    public interface IFileService
+    {
+        /// <summary>
+        /// Create Image
+        /// </summary>
+        /// <param name="img"></param>
+        /// <returns></returns>
+        Task<string> CreateImage(IFormFile img);
+
+        /// <summary>
+        /// Upload Video
+        /// </summary>
+        /// <param name="media"></param>
+        /// <returns></returns>
+        Task<string> UploadVideo(IFormFile media);
+
+
+        /// <summary>
+        /// Delete Image
+        /// </summary>
+        /// <param name="url"></param>
+        Task<string> DeleteImage(string url);
+    }
+}
